f16ef28223b7329053b05d1fe8585409ee177ab3
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-02-03 01:01:25 +0200

    responsive UI Popular matches collection view done

--- FanZone/View/HomeVC.swift
+++ FanZone/View/HomeVC.swift
@@ -13,23 +13,30 @@ class HomeVC: UIViewController {
 
 
     @IBOutlet weak var leagueCollectionView: UICollectionView!
+    @IBOutlet weak var popularMatchesCollectionView: UICollectionView!
+    
     
     private let disposeBag = DisposeBag()
     var arr = ["PL","laliga","EPL2","BL","CL","SA"]
     
+    var stadArray = ["Stad1","Stad2","Stad3"]
+    var homeTeamArray = ["Arsenal","Manchester United", "Nottengham Forest"]
+    var awayTeamArray = ["Manchester United", "Manchester City","Wolves"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         NavBar.applyCustomNavBar(to: self)
         leagueCollectionView.register(UINib(nibName: "LeagueCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "leagueCell")
+        popularMatchesCollectionView.register(UINib(nibName: "PopularCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "popularCell")
         setUpLeagueCollectionView()
+        setUpPopularMatchesCollectionView()
 
     }
 
 }
 
 extension HomeVC{
-    
     func setUpLeagueCollectionView(){
         Observable.just(arr)
             .bind(to: leagueCollectionView.rx.items(cellIdentifier: "leagueCell", cellType: LeagueCollectionViewCell.self)){row,image,cell in
@@ -41,6 +48,34 @@ extension HomeVC{
                 
                 cell.leagueImageView.tintColor = UIColor(red: 33/255, green: 53/255, blue: 85/255, alpha: 1)
                 cell.leagueImageView.image = UIImage(named: image)?.withRenderingMode(.alwaysTemplate)
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension HomeVC{
+    func setUpPopularMatchesCollectionView(){
+        Observable.just(stadArray)
+            .bind(to: popularMatchesCollectionView.rx.items(cellIdentifier: "popularCell", cellType: PopularCollectionViewCell.self)){row,stad,cell in
+                
+                cell.stadiumImageView.image = UIImage(named: stad)
+                
+                if row < self.homeTeamArray.count {
+                    cell.homeTeam.text = self.homeTeamArray[row]
+                }
+                if row < self.awayTeamArray.count {
+                    cell.awayTeam.text = self.awayTeamArray[row]
+                }
+                
+                cell.matchDate.text = "2024-02-02"
+                cell.stadiumName.text = "Etihad Stadium"
+                
+                // Add a light grey stroke to the cell
+                cell.contentView.layer.borderWidth = 1.0
+                cell.contentView.layer.borderColor = UIColor.lightGray.cgColor
+                cell.contentView.layer.cornerRadius = 10.0 // Adjust the corner radius as needed
+                cell.contentView.layer.masksToBounds = true
+                
             }
             .disposed(by: disposeBag)
     }
---
+++ FanZone/View/PopularCollectionViewCell/PopularCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PopularCollectionViewCell.swift
+//  FanZone
+//
+//  Created by Mahmoud Mohamed Atrees on 02/02/2024.
+//
+
+import UIKit
+
+class PopularCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var stadiumImageView: UIImageView!
+    
+    @IBOutlet weak var homeTeam: UILabel!
+    @IBOutlet weak var awayTeam: UILabel!
+    
+    @IBOutlet weak var matchDate: UILabel!
+    @IBOutlet weak var stadiumName: UILabel!
+    
+    @IBOutlet weak var getTicketButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        getTicketButton.tintColor = UIColor(red: 33/255, green: 53/255, blue: 85/255, alpha: 1.0)
+    }
+
+    @IBAction func getTicketButtonPressed(_ sender: UIButton) {
+    }
+}
