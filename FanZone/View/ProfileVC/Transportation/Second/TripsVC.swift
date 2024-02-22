9dcb9dbc996fd684af2d1534306bbcc698b27c2e
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-02-22 16:16:23 +0200

    some updates

---
+++ FanZone/View/ProfileVC/Transportation/Second/TripsVC.swift
@@ -0,0 +1,18 @@
+//
+//  TripsVC.swift
+//  FanZone
+//
+//  Created by Mahmoud Mohamed Atrees on 22/02/2024.
+//
+
+import UIKit
+
+class TripsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
